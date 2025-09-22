f2a58b7a82ab1e457f43db1e5913a3420de1c039
N1kita14 <nikita141299.n@yandex.ru>
2025-09-22 19:58:50 +0300

    fix RootController.java

--- app/src/main/java/hexlet/code/controller/RootController.java
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -4,6 +4,6 @@ import io.javalin.http.Context;
 
 public class RootController {
     public static void index(Context ctx) {
-        ctx.render("page.jte");
+        ctx.render("index.jte");
     }
 }
